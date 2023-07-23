e7284be6e84ceed2748189d36297b7382c37c060
jeromel <jeromal03@essentials>
2023-07-23 04:35:47 +0000

    breaked code

--- _templates/_footer.php
+++ _templates/_footer.php
@@ -1,3 +1,4 @@
+<footer class="text-muted py-5">
 	<div class="container">
 		<p class="float-end mb-1">
 			<a href="#">Back to top</a>
@@ -6,3 +7,4 @@
 		<p class="mb-0">New to Bootstrap? <a href="/">Visit the homepage</a> or read our <a
 				href="../getting-started/introduction/">getting started guide</a>.</p>
 	</div>
+</footer>
